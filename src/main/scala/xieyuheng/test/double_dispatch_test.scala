3913bec92f42fe8bc77a201f0c67fd1c18c75623
Xie Yuheng <xyheme@gmail.com>
2019-12-26 19:18:05 +0800

    up

--- src/main/scala/xieyuheng/test/double_dispatch_test.scala
+++ src/main/scala/xieyuheng/test/double_dispatch_test.scala
@@ -3,43 +3,43 @@ package xieyuheng.test
 object double_dispatch_test extends App {
 
   trait entity_t {
-    def collide(that: entity_t): Unit
+    def collide(that: entity_t): String
   }
 
   case class entity_one_t() extends entity_t {
-    def collide(that: entity_t): Unit = {
+    def collide(that: entity_t): String = {
       that.collide(this)
     }
 
-    def collide(that: entity_one_t): Unit = {
-      println("entity_one_t entity_one_t")
+    def collide(that: entity_one_t): String = {
+      "entity_one_t entity_one_t"
     }
 
-    def collide(that: entity_two_t): Unit = {
-      println("entity_one_t entity_two_t")
+    def collide(that: entity_two_t): String = {
+      "entity_one_t entity_two_t"
     }
   }
 
   case class entity_two_t() extends entity_t {
-    def collide(that: entity_t): Unit = {
+    def collide(that: entity_t): String = {
       that.collide(this)
     }
 
-    def collide(that: entity_one_t): Unit = {
-      println("entity_two_t entity_one_t")
+    def collide(that: entity_one_t): String = {
+      "entity_two_t entity_one_t"
     }
 
-    def collide(that: entity_two_t): Unit = {
-      println("entity_two_t entity_two_t")
+    def collide(that: entity_two_t): String = {
+      "entity_two_t entity_two_t"
     }
   }
 
   val entity_one = entity_one_t()
   val entity_two = entity_two_t()
 
-  entity_one.collide(entity_one)
-  entity_one.collide(entity_two)
-  entity_two.collide(entity_one)
-  entity_two.collide(entity_two)
+  assert(entity_one.collide(entity_one) == "entity_one_t entity_one_t")
+  assert(entity_one.collide(entity_two) == "entity_one_t entity_two_t")
+  assert(entity_two.collide(entity_one) == "entity_two_t entity_one_t")
+  assert(entity_two.collide(entity_two) == "entity_two_t entity_two_t")
 
 }
