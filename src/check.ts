5511b9cd93af221cf230a4dd0ec373096248bed3
Xie Yuheng <xyheme@gmail.com>
2020-01-30 23:10:09 +0800

    note about `unique_var` in `check`

--- src/check.ts
+++ src/check.ts
@@ -182,32 +182,28 @@ export function check_fn(
     let scope_env = pi.scope_env
     for (let i = 0; i < scope.arity; i++) {
       // TODO scope of pi should also be able to contain let and define
-      //   we should not use Scope.entry_to_type
+      //   we should not use `Scope.entry_to_type`
       let [pi_arg_name, pi_arg_entry] = pi.scope.named_entries[i]
       let [fn_arg_name, fn_arg_entry] = scope.named_entries[i]
       let pi_arg_type_value = Scope.entry_to_type(pi_arg_entry, scope_env)
       let fn_arg_type_value = Scope.entry_to_type(fn_arg_entry, local_env)
       subtype(fn_arg_type_value, pi_arg_type_value)
 
-      local_env = local_env.ext(fn_arg_name, {
+      // NOTE we need to use unique_var as unification here,
+      //   because equivalent between `Value.Neutral.Var` will only compare name.
+
+      let unique_var = util.unique_var_from(
+        `check:Fn:${pi_arg_name}:${fn_arg_name}`)
+
+      let the = {
         t: fn_arg_type_value,
-        value: new Value.The(fn_arg_type_value, new Value.Neutral.Var(fn_arg_name)),
-      })
+        value: new Value.The(fn_arg_type_value, unique_var),
+      }
 
-      scope_env = scope_env.ext(pi_arg_name, {
-        t: fn_arg_type_value,
-        value: new Value.The(fn_arg_type_value, new Value.Neutral.Var(pi_arg_name)),
-      })
-
-      // NOTE we need to use unique_var as unification here
-
-      // TODO
-      // let unique_var = util.unique_var_from(
-      //   `check:Fn:${pi_arg_name}:${fn_arg_name}`)
-      // local_env = local_env.ext(fn_arg_name, { t: fn_arg_type_value, value: unique_var })
-      // scope_env = scope_env.ext(pi_arg_name, { t: fn_arg_type_value, value: unique_var })
-      // local_env = local_env.ext(unique_var.name, { t: fn_arg_type_value, value: unique_var })
-      // scope_env = scope_env.ext(unique_var.name, { t: fn_arg_type_value, value: unique_var })
+      local_env = local_env.ext(fn_arg_name, the)
+      scope_env = scope_env.ext(pi_arg_name, the)
+      local_env = local_env.ext(unique_var.name, the)
+      scope_env = scope_env.ext(unique_var.name, the)
     }
     check(local_env, body, evaluate(scope_env, pi.return_type))
   }
