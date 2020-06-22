b689e73db4fb54885225bd18e4e0ce0674a29efd
Xie Yuheng <xyheme@gmail.com>
2020-06-22 23:59:07 +0800

    - freshen

--- src/untyped/env/env-extend.ts
+++ src/untyped/env/env-extend.ts
@@ -1,7 +1,8 @@
 import * as Env from "../env"
 import * as Value from "../value"
 
-// NOTE side effect API, one need to clone the env as needed.
+// NOTE side effect API,
+//   one needs to clone the env as needed.
 export function extend(env: Env.Env, name: string, value: Value.Value): Env.Env {
   env.set(name, value)
   return env
