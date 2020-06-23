25e8b4094f1de4694e3008358c3840fc1f7a4d24
Xie Yuheng <xyheme@gmail.com>
2020-06-23 21:41:54 +0800

    up

--- src/untyped/cli/cli-eval.ts
+++ src/untyped/cli/cli-eval.ts
@@ -4,5 +4,5 @@ import fs from "fs"
 export function run(file: string, opts: any): void {
   const text = fs.readFileSync(file, { encoding: "utf-8" })
   const exp = Exp.parse(text)
-  console.log(Exp.normalize(exp))
+  console.log(Exp.repr(Exp.normalize(exp)))
 }
