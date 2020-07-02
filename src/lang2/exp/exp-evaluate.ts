6bc6875a20c9062bcbdaeebe0812a9d527592dd7
Xie Yuheng <xyheme@gmail.com>
2020-07-02 14:15:43 +0800

    up

--- src/lang2/exp/exp-evaluate.ts
+++ src/lang2/exp/exp-evaluate.ts
@@ -38,24 +38,78 @@ export function evaluate(env: Env.Env, exp: Exp.Exp): Value.Value {
         const { rator, rand } = exp
         return Exp.do_ap(evaluate(env, rator), evaluate(env, rand))
       }
-      case "Exp.Sigma":
-      case "Exp.Cons":
+      case "Exp.Sigma": {
+        return {
+          kind: "Value.Sigma",
+          car_t: Exp.evaluate(env, exp.car_t),
+          closure: new Closure.Closure(env, exp.name, exp.cdr_t),
+        }
+      }
+      case "Exp.Cons": {
+        return {
+          kind: "Value.Cons",
+          car: Exp.evaluate(env, exp.car),
+          cdr: Exp.evaluate(env, exp.cdr),
+        }
+      }
       case "Exp.Car":
       case "Exp.Cdr":
-      case "Exp.Nat":
-      case "Exp.Zero":
-      case "Exp.Succ":
+      case "Exp.Nat": {
+        return {
+          kind: "Value.Nat",
+        }
+      }
+      case "Exp.Zero": {
+        return {
+          kind: "Value.Zero",
+        }
+      }
+      case "Exp.Succ": {
+        return {
+          kind: "Value.Succ",
+          prev: Exp.evaluate(env, exp.prev),
+        }
+      }
       case "Exp.NatInd":
       case "Exp.Equal":
-      case "Exp.Same":
+      case "Exp.Same": {
+        return {
+          kind: "Value.Same",
+        }
+      }
       case "Exp.Replace":
-      case "Exp.Trivial":
-      case "Exp.Sole":
-      case "Exp.Absurd":
+      case "Exp.Trivial": {
+        return {
+          kind: "Value.Trivial",
+        }
+      }
+      case "Exp.Sole": {
+        return {
+          kind: "Value.Sole",
+        }
+      }
+      case "Exp.Absurd": {
+        return {
+          kind: "Value.Absurd",
+        }
+      }
       case "Exp.AbsurdInd":
-      case "Exp.Str":
-      case "Exp.Quote":
-      case "Exp.Type":
+      case "Exp.Str": {
+        return {
+          kind: "Value.Str",
+        }
+      }
+      case "Exp.Quote": {
+        return {
+          kind: "Value.Quote",
+          str: exp.str,
+        }
+      }
+      case "Exp.Type": {
+        return {
+          kind: "Value.Type",
+        }
+      }
       case "Exp.Suite":
       case "Exp.The": {
         throw new Error("TODO")
