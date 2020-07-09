8b2031e8bdf5e84df3c37c582a9afbdd75d2ef0d
Xie Yuheng <xyheme@gmail.com>
2020-07-09 20:39:02 +0800

    - Exp.check

--- kanban/todo.md
+++ kanban/todo.md
@@ -1,6 +1,6 @@
 # tartlet
 
-- Exp.check
+
 
 # pie
 
--- src/lang2/exp/exp-check.ts
+++ src/lang2/exp/exp-check.ts
@@ -7,5 +7,54 @@ import * as Ctx from "../ctx"
 import * as Trace from "../../trace"
 
 export function check(ctx: Ctx.Ctx, exp: Exp.Exp, t: Ty.Ty): void {
-  throw new Error("TODO")
+  try {
+    if (exp.kind === "Exp.Fn") {
+      // ctx, x: arg_t |- body <= ret_t
+      // ---------------------------
+      // ctx |- (x) => body  <=  (x: arg_t) -> ret_t
+      const pi = Value.isPi(ctx, t)
+      const ret_t = Closure.apply(pi.closure, {
+        kind: "Value.Reflection",
+        t: pi.arg_t,
+        neutral: { kind: "Neutral.Var", name: exp.name },
+      })
+      ctx = Ctx.clone(ctx)
+      ctx = Ctx.extend(ctx, exp.name, pi.arg_t)
+      Exp.check(ctx, exp.body, ret_t)
+    } else if (exp.kind === "Exp.Cons") {
+      // ctx |- car <= car_t
+      // ctx |- cdr <= cdr_t[car/x]
+      // -------------------------
+      // ctx |- Cons(car, cdr) <= (x: car_t) * cdr_t
+      const sigma = Value.isSigma(ctx, t)
+      const car = Exp.evaluate(Ctx.to_env(ctx), exp.car)
+      const cdr_t = Closure.apply(sigma.closure, car)
+      Exp.check(ctx, exp.car, sigma.car_t)
+      Exp.check(ctx, exp.cdr, cdr_t)
+    } else if (exp.kind === "Exp.Zero") {
+      Value.isNat(ctx, t)
+    } else if (exp.kind === "Exp.Succ") {
+      Value.isNat(ctx, t)
+      Exp.check(ctx, exp.prev, t)
+    } else if (exp.kind === "Exp.Same") {
+      // ctx |- from == to : t
+      // ------------------------
+      // ctx |- Same <= Equal(t, from, to)
+      const equal = Value.isEqual(ctx, t)
+      Value.convert(ctx, equal.t, equal.from, equal.to)
+    } else if (exp.kind === "Exp.Sole") {
+      Value.isTrivial(ctx, t)
+    } else if (exp.kind === "Exp.Quote") {
+      Value.isStr(ctx, t)
+    } else {
+      // ctx |- exp => u
+      // ctx |- t == u : Type
+      // ----------------------
+      // ctx |- exp <= t
+      const u = Exp.infer(ctx, exp)
+      Value.convert(ctx, { kind: "Value.Type" }, t, u)
+    }
+  } catch (error) {
+    Trace.maybe_push(error, exp)
+  }
 }
