e844c3d99729363c2aa865687eff72e5f34b3746
Xie Yuheng <xyheme@gmail.com>
2020-09-13 20:04:23 +0800

    up

--- scripts/expect.js
+++ scripts/expect.js
@@ -1,25 +1,24 @@
 const execute = require("./execute")
 const chalk = require("chalk")
 
-async function ok(prog, { echo } = {}) {
+async function ok(prog) {
   execute(prog).then(({ stdout, stderr, error }) => {
     const head = chalk.bold.blue("[expect.ok]")
     console.log(`${head} ${prog}`)
-    if (stdout && echo) console.log(stdout)
-    if (stderr && echo) console.error(stderr)
+    if (stdout) console.log(stdout)
+    if (stderr) console.error(stderr)
     if (error) {
-      console.error(error.message)
       process.exit(1)
     }
   })
 }
 
-async function fail(prog, { echo } = {}) {
+async function fail(prog) {
   execute(prog).then(({ stdout, stderr }) => {
     const head = chalk.bold.red("[expect.fail]")
     console.log(`${head} ${prog}`)
-    if (stdout && echo) console.log(stdout)
-    if (stderr && echo) console.error(stderr)
+    console.log(stdout)
+    console.error(stderr)
   })
 }
 
