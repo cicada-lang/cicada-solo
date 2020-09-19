34745c01d470ee4d14a2d12f6bda194a413a90ac
Xie Yuheng <xyheme@gmail.com>
2020-09-19 19:55:30 +0800

    up

--- src/pt/closure/closure.ts
+++ src/pt/closure/closure.ts
@@ -1,8 +1,10 @@
 import * as Env from "../env"
+import * as Mod from "../mod"
 import * as Exp from "../exp"
 
 export interface Closure {
   name: string
   exp: Exp.Exp
   env: Env.Env
+  mod: Mod.Mod
 }
