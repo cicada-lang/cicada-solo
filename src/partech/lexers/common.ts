0c2306878beb4c6b57d5660112b4df90ca25ff9c
Xie Yuheng <xyheme@gmail.com>
2020-10-01 11:35:31 +0800

    - examples for json

--- src/partech/lexers/common.ts
+++ src/partech/lexers/common.ts
@@ -3,6 +3,6 @@ import * as TableLexer from "../table-lexer"
 export const common = TableLexer.build([
   ["identifier", "^\\s*([_\\p{Letter}][_\\p{Letter}0-9]*)\\s*", "u"],
   ["string", '^\\s*("(\\\\.|[^"])*")\\s*'],
-  ["number", "^\\s*(\\d+|\\d+\\.\\d+|-\\d+|-\\d+\\.\\d+)\\s*"],
-  ["symbol", "^\\s*([^_\\p{Letter}0-9\\s]+)\\s*", "u"],
+  ["number", "^\\s*(\\d+\\.\\d+|\\d+|-\\d+\\.\\d+|-\\d+)\\s*"],
+  ["symbol", "^\\s*([^_\\p{Letter}0-9\\s])\\s*", "u"],
 ])
