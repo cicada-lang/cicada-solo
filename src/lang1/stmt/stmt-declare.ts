40310d5920c878640992bbada10a6c1f6d018ecf
Xie Yuheng <xyheme@gmail.com>
2020-10-05 22:32:06 +0800

    - [lang1] `Stmt.declare`

---
+++ src/lang1/stmt/stmt-declare.ts
@@ -0,0 +1,12 @@
+import * as Stmt from "../stmt"
+import * as Ctx from "../ctx"
+import * as Exp from "../exp"
+
+export function declare(ctx: Ctx.Ctx, stmt: Stmt.Stmt): void {
+  switch (stmt.kind) {
+    case "Stmt.def": {
+      const { name, exp } = stmt
+      Ctx.update(ctx, name, Exp.infer(ctx, exp))
+    }
+  }
+}
