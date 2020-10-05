7fcc8f25c797a1c0ca1e53853eb68558a7142da5
Xie Yuheng <xyheme@gmail.com>
2020-10-05 22:36:31 +0800

    - [lang0] `Stmt.top`

---
+++ src/lang0/stmt/stmt-top.ts
@@ -0,0 +1,15 @@
+import * as Stmt from "../stmt"
+import * as Exp from "../exp"
+import * as Value from "../value"
+import * as Env from "../env"
+
+export function top(env: Env.Env, stmt: Stmt.Stmt): void {
+  Stmt.execute(env, stmt)
+
+  switch (stmt.kind) {
+    case "Stmt.show": {
+      const { exp } = stmt
+      console.log(Exp.repr(Exp.normalize(exp)))
+    }
+  }
+}
