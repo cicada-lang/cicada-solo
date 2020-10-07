36b5a82d46adaa2822f5e9f31e54852bf28bc60c
Xie Yuheng <xyheme@gmail.com>
2020-10-07 17:18:03 +0800

    - [lang3] telescope-create.ts

--- src/lang3/telescope/index.ts
+++ src/lang3/telescope/index.ts
@@ -1,1 +1,2 @@
 export * from "./telescope"
+export * from "./telescope-create"
---
+++ src/lang3/telescope/telescope-create.ts
@@ -0,0 +1,18 @@
+import * as Telescope from "../telescope"
+import * as Exp from "../exp"
+import * as Value from "../value"
+import * as Queue from "../../queue"
+
+export function create(
+  env: Map<string, Value.Value>,
+  satisfied: Map<string, { t: Value.Value; value: Value.Value }>,
+  next: undefined | { name: string; t: Value.Value },
+  queue: Queue.Queue<{ name: string; exp: Exp.Exp }>
+): Telescope.Telescope {
+  return {
+    env,
+    satisfied,
+    next,
+    queue,
+  }
+}
