c9d6d7fd5c538c45f2476249b04713353d954680
Xie Yuheng <xyheme@gmail.com>
2020-10-07 17:15:30 +0800

    - queue-create.ts

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,1 +1,2 @@
 export * from "./queue"
+export * from "./queue-create"
---
+++ src/queue/queue-create.ts
@@ -0,0 +1,5 @@
+import * as Queue from "../queue"
+
+export function create<A>(elements: Array<A>): Queue.Queue<A> {
+  return elements
+}
