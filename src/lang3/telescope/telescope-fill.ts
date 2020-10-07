19cb77eea9d4a263873c430ff99ca992bfc3060e
Xie Yuheng <xyheme@gmail.com>
2020-10-07 23:56:30 +0800

    - [lang3] telescope-fill.ts

---
+++ src/lang3/telescope/telescope-fill.ts
@@ -0,0 +1,28 @@
+import * as Telescope from "../telescope"
+import * as Env from "../env"
+import * as Exp from "../exp"
+import * as Value from "../value"
+import * as Neutral from "../neutral"
+import * as Trace from "../../trace"
+import * as ut from "../../ut"
+
+export function fill(
+  tel: Telescope.Telescope,
+  value: Value.Value
+): Telescope.Telescope {
+  let { env, sat, next, queue } = tel
+  if (next === undefined)
+    throw new Trace.Trace(
+      ut.aline(`
+        |Filling fulled telescope.
+        |- telescope: ${ut.inspect(tel)}
+        |- value: ${ut.inspect(value)}
+        |`)
+    )
+
+  env = Env.extend(env, next.name, value)
+  sat = [...sat, { name: next.name, t: next.t, value }]
+  const [entry, ...rest] = queue
+  const t = Exp.evaluate(env, entry.t)
+  return Telescope.create(env, sat, { name: entry.name, t }, rest)
+}
