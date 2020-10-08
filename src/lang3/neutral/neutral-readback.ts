f7acc4c95327869913c324751fc0a540f7c1f00b
Xie Yuheng <xyheme@gmail.com>
2020-10-08 14:54:49 +0800

    - [lang3] neutral-readback.ts

--- src/lang3/neutral/neutral-readback.ts
+++ src/lang3/neutral/neutral-readback.ts
@@ -4,43 +4,31 @@ import * as Exp from "../exp"
 import * as Ctx from "../ctx"
 
 export function readback(ctx: Ctx.Ctx, neutral: Neutral.Neutral): Exp.Exp {
-  throw new Error("TODO")
-  // switch (neutral.kind) {
-  //   case "Neutral.v": {
-  //     return Exp.v(neutral.name)
-  //   }
-  //   case "Neutral.ap": {
-  //     return Exp.ap(
-  //       Neutral.readback(ctx, neutral.target),
-  //       Normal.readback(ctx, neutral.arg)
-  //     )
-  //   }
-  //   case "Neutral.car": {
-  //     return Exp.car(Neutral.readback(ctx, neutral.target))
-  //   }
-  //   case "Neutral.cdr": {
-  //     return Exp.cdr(Neutral.readback(ctx, neutral.target))
-  //   }
-  //   case "Neutral.nat_ind": {
-  //     return Exp.nat_ind(
-  //       Neutral.readback(ctx, neutral.target),
-  //       Normal.readback(ctx, neutral.motive),
-  //       Normal.readback(ctx, neutral.base),
-  //       Normal.readback(ctx, neutral.step)
-  //     )
-  //   }
-  //   case "Neutral.replace": {
-  //     return Exp.replace(
-  //       Neutral.readback(ctx, neutral.target),
-  //       Normal.readback(ctx, neutral.motive),
-  //       Normal.readback(ctx, neutral.base)
-  //     )
-  //   }
-  //   case "Neutral.absurd_ind": {
-  //     return Exp.absurd_ind(
-  //       Neutral.readback(ctx, neutral.target),
-  //       Normal.readback(ctx, neutral.motive)
-  //     )
-  //   }
-  // }
+  switch (neutral.kind) {
+    case "Neutral.v": {
+      return Exp.v(neutral.name)
+    }
+    case "Neutral.ap": {
+      return Exp.ap(
+        Neutral.readback(ctx, neutral.target),
+        Normal.readback(ctx, neutral.arg)
+      )
+    }
+    case "Neutral.dot": {
+      throw new Error("TODO")
+    }
+    case "Neutral.replace": {
+      return Exp.replace(
+        Neutral.readback(ctx, neutral.target),
+        Normal.readback(ctx, neutral.motive),
+        Normal.readback(ctx, neutral.base)
+      )
+    }
+    case "Neutral.absurd_ind": {
+      return Exp.absurd_ind(
+        Neutral.readback(ctx, neutral.target),
+        Normal.readback(ctx, neutral.motive)
+      )
+    }
+  }
 }
