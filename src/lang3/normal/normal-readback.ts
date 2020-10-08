84253e40b9ef861e828df6e08f08a059109e3475
Xie Yuheng <xyheme@gmail.com>
2020-10-08 14:45:30 +0800

    - [lang3] normal-readback.ts

---
+++ src/lang3/normal/normal-readback.ts
@@ -0,0 +1,8 @@
+import * as Normal from "../normal"
+import * as Value from "../value"
+import * as Exp from "../exp"
+import * as Ctx from "../ctx"
+
+export function readback(ctx: Ctx.Ctx, normal: Normal.Normal): Exp.Exp {
+  return Value.readback(ctx, normal.t, normal.value)
+}
