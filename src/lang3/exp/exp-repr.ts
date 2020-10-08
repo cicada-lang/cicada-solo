f2b271a96ad15b05726ab08b4d43a6ce5e5c6a89
Xie Yuheng <xyheme@gmail.com>
2020-10-08 12:36:15 +0800

    up

---
+++ src/lang3/exp/exp-repr.ts
@@ -0,0 +1,68 @@
+import * as Exp from "../exp"
+import * as Stmt from "../stmt"
+import * as ut from "../../ut"
+
+export function repr(exp: Exp.Exp): string {
+  switch (exp.kind) {
+    case "Exp.v": {
+      return exp.name
+    }
+    case "Exp.pi": {
+      return `(${exp.name}: ${Exp.repr(exp.arg_t)}) -> ${Exp.repr(exp.ret_t)}`
+    }
+    case "Exp.fn": {
+      return `(${exp.name}) => ${Exp.repr(exp.ret)}`
+    }
+    case "Exp.ap": {
+      return `${Exp.repr(exp.target)}(${Exp.repr(exp.arg)})`
+    }
+    case "Exp.cls": {
+      throw new Error("TODO")
+    }
+    case "Exp.fill": {
+      throw new Error("TODO")
+    }
+    case "Exp.obj": {
+      throw new Error("TODO")
+    }
+    case "Exp.dot": {
+      throw new Error("TODO")
+    }
+    case "Exp.equal": {
+      return `Equal(${Exp.repr(exp.t)}, ${Exp.repr(exp.from)}, ${Exp.repr(
+        exp.to
+      )})`
+    }
+    case "Exp.same": {
+      return "same"
+    }
+    case "Exp.replace": {
+      return `replace(${Exp.repr(exp.target)}, ${Exp.repr(
+        exp.motive
+      )}, ${Exp.repr(exp.base)})`
+    }
+    case "Exp.absurd": {
+      return "Absurd"
+    }
+    case "Exp.absurd_ind": {
+      return `Absurd.ind(${Exp.repr(exp.target)}, ${Exp.repr(exp.motive)})`
+    }
+    case "Exp.str": {
+      return "String"
+    }
+    case "Exp.quote": {
+      return `"${exp.str}"`
+    }
+    case "Exp.type": {
+      return "Type"
+    }
+    case "Exp.suite": {
+      throw new Error()
+      // const s = [...exp.stmts.map(Stmt.repr), repr(exp.ret)].join("\n")
+      // return `{\n${ut.indent(s, "  ")}\n}`
+    }
+    case "Exp.the": {
+      return `${Exp.repr(exp.exp)}: ${Exp.repr(exp.t)}`
+    }
+  }
+}
