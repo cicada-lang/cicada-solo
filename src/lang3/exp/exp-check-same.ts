11a82ed28e5ae4e49da69630effa4915d69cca93
Xie Yuheng <xyheme@gmail.com>
2020-10-11 23:07:27 +0800

    - [lang3] exp-check-same.ts

---
+++ src/lang3/exp/exp-check-same.ts
@@ -0,0 +1,26 @@
+import * as Exp from "../exp"
+import * as Stmt from "../stmt"
+import * as Value from "../value"
+import * as Neutral from "../neutral"
+import * as Closure from "../closure"
+import * as Telescope from "../telescope"
+import * as Ctx from "../ctx"
+import * as Trace from "../../trace"
+import * as ut from "../../ut"
+
+export function check_same(ctx: Ctx.Ctx, same: Exp.same, equal: Value.equal): void {
+  if (!Value.conversion(ctx, equal.t, equal.from, equal.to)) {
+    throw new Trace.Trace(
+      ut.aline(`
+          |I am expecting the following two values to be the same ${Exp.repr(
+            Value.readback(ctx, Value.type, equal.t)
+          )}.
+          |But they are not.
+          |from:
+          |  ${Exp.repr(Value.readback(ctx, equal.t, equal.from))}
+          |to:
+          |  ${Exp.repr(Value.readback(ctx, equal.t, equal.to))}
+          |`)
+    )
+  }
+}
--- src/lang3/exp/exp-check.ts
+++ src/lang3/exp/exp-check.ts
@@ -9,6 +9,7 @@ import * as Trace from "../../trace"
 import * as ut from "../../ut"
 import { check_fn } from "./exp-check-fn"
 import { check_obj } from "./exp-check-obj"
+import { check_same } from "./exp-check-same"
 import { check_quote } from "./exp-check-quote"
 import { check_union_type } from "./exp-check-union-type"
 import { check_by_infer } from "./exp-check-by-infer"
@@ -22,21 +23,7 @@ export function check(ctx: Ctx.Ctx, exp: Exp.Exp, t: Value.Value): void {
     } else if (exp.kind === "Exp.obj") {
       check_obj(ctx, exp, Value.is_cls(ctx, t))
     } else if (exp.kind === "Exp.same") {
-      const equal = Value.is_equal(ctx, t)
-      if (!Value.conversion(ctx, equal.t, equal.from, equal.to)) {
-        throw new Trace.Trace(
-          ut.aline(`
-          |I am expecting the following two values to be the same ${Exp.repr(
-            Value.readback(ctx, Value.type, equal.t)
-          )}.
-          |But they are not.
-          |from:
-          |  ${Exp.repr(Value.readback(ctx, equal.t, equal.from))}
-          |to:
-          |  ${Exp.repr(Value.readback(ctx, equal.t, equal.to))}
-          |`)
-        )
-      }
+      check_same(ctx, exp, Value.is_equal(ctx, t))
     } else if (exp.kind === "Exp.begin") {
       const { stmts, ret } = exp
       ctx = Ctx.clone(ctx)
