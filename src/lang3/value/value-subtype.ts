7acc2633aa489145c53f1640bb7526a509c150df
Xie Yuheng <xyheme@gmail.com>
2020-10-11 21:44:33 +0800

    - [lang3] `Value.subtype` for `Value.union`

--- src/lang3/value/value-subtype.ts
+++ src/lang3/value/value-subtype.ts
@@ -3,8 +3,11 @@ import * as Exp from "../exp"
 import * as Ctx from "../ctx"
 
 export function subtype(ctx: Ctx.Ctx, x: Value.Value, y: Value.Value): boolean {
-  return (
-    Value.conversion(ctx, Value.type, x, y) ||
-    (x.kind === "Value.quote" && y.kind === "Value.str")
-  )
+  if (Value.conversion(ctx, Value.type, x, y)) return true
+  else if (x.kind === "Value.quote" && y.kind === "Value.str") return true
+  else if (x.kind === "Value.union")
+    return Value.subtype(ctx, x.left, y) && Value.subtype(ctx, x.right, y)
+  else if (y.kind === "Value.union")
+    return Value.subtype(ctx, x, y.left) || Value.subtype(ctx, x, y.right)
+  else return false
 }
