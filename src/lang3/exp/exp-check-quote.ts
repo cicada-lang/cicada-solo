894bf2a5b2bd60fb81192571460cd0219a4d5d00
Xie Yuheng <xyheme@gmail.com>
2020-10-12 12:04:18 +0800

    - [lang3] consolidate conditional expression `check_quote`

--- src/lang3/exp/exp-check-quote.ts
+++ src/lang3/exp/exp-check-quote.ts
@@ -9,30 +9,12 @@ export function check_quote(
   quote: Exp.quote,
   t: Value.Value
 ): void {
-  if (t.kind === "Value.type") {
-    // NOTE literal string type
-    return
-  } else if (t.kind === "Value.str") {
-    return
-  } else if (t.kind === "Value.quote") {
-    if (quote.str === t.str) {
-      return
-    } else {
-      throw new Trace.Trace(
-        ut.aline(`
-          |The given value is string: ${Exp.repr(quote)},
-          |But the given type is ${Exp.repr(
-            Value.readback(ctx, Value.type, t)
-          )}.
-          |`)
-      )
-    }
-  } else {
-    throw new Trace.Trace(
-      ut.aline(`
-        |The given value is string: ${Exp.repr(quote)},
-        |But the given type is ${Exp.repr(Value.readback(ctx, Value.type, t))}.
-        |`)
-    )
-  }
+  if (t.kind === "Value.type" || t.kind === "Value.str") return
+  if (t.kind === "Value.quote" && quote.str === t.str) return
+  throw new Trace.Trace(
+    ut.aline(`
+      |The given value is string: ${Exp.repr(quote)},
+      |But the given type is ${Exp.repr(Value.readback(ctx, Value.type, t))}.
+      |`)
+  )
 }
