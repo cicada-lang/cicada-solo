2d015affd247afbcea717488a0ed7a9d8589762c
Xie Yuheng <xyheme@gmail.com>
2020-10-12 22:14:54 +0800

    up

--- src/lang3/mod/mod-lookup-type.ts
+++ src/lang3/mod/mod-lookup-type.ts
@@ -3,7 +3,10 @@ import * as Ctx from "../ctx"
 import * as Exp from "../exp"
 import * as Value from "../value"
 
-export function lookup_type(mod: Mod.Mod, name: string): undefined | Value.Value {
+export function lookup_type(
+  mod: Mod.Mod,
+  name: string
+): undefined | Value.Value {
   const exp = mod.map.get(name)
   if (exp === undefined) return undefined
   // NOTE maybe use try and Trace for `Exp.infer`
