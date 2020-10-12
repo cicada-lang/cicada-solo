622ec6c5829b675c4ede80857e5557f2cad930ff
Xie Yuheng <xyheme@gmail.com>
2020-10-12 19:10:50 +0800

    - [lang3] `Mod` in `Neutral.readback` & `Normal.readback`

--- src/lang3/neutral/neutral-readback.ts
+++ src/lang3/neutral/neutral-readback.ts
@@ -2,32 +2,33 @@ import * as Neutral from "../neutral"
 import * as Normal from "../normal"
 import * as Exp from "../exp"
 import * as Ctx from "../ctx"
+import * as Mod from "../mod"
 
-export function readback(ctx: Ctx.Ctx, neutral: Neutral.Neutral): Exp.Exp {
+export function readback(mod: Mod.Mod, ctx: Ctx.Ctx, neutral: Neutral.Neutral): Exp.Exp {
   switch (neutral.kind) {
     case "Neutral.v": {
       return Exp.v(neutral.name)
     }
     case "Neutral.ap": {
       return Exp.ap(
-        Neutral.readback(ctx, neutral.target),
-        Normal.readback(ctx, neutral.arg)
+        Neutral.readback(mod, ctx, neutral.target),
+        Normal.readback(mod, ctx, neutral.arg)
       )
     }
     case "Neutral.dot": {
-      return Exp.dot(Neutral.readback(ctx, neutral.target), neutral.name)
+      return Exp.dot(Neutral.readback(mod, ctx, neutral.target), neutral.name)
     }
     case "Neutral.replace": {
       return Exp.replace(
-        Neutral.readback(ctx, neutral.target),
-        Normal.readback(ctx, neutral.motive),
-        Normal.readback(ctx, neutral.base)
+        Neutral.readback(mod, ctx, neutral.target),
+        Normal.readback(mod, ctx, neutral.motive),
+        Normal.readback(mod, ctx, neutral.base)
       )
     }
     case "Neutral.absurd_ind": {
       return Exp.absurd_ind(
-        Neutral.readback(ctx, neutral.target),
-        Normal.readback(ctx, neutral.motive)
+        Neutral.readback(mod, ctx, neutral.target),
+        Normal.readback(mod, ctx, neutral.motive)
       )
     }
   }
--- src/lang3/normal/normal-readback.ts
+++ src/lang3/normal/normal-readback.ts
@@ -2,7 +2,8 @@ import * as Normal from "../normal"
 import * as Value from "../value"
 import * as Exp from "../exp"
 import * as Ctx from "../ctx"
+import * as Mod from "../mod"
 
-export function readback(ctx: Ctx.Ctx, normal: Normal.Normal): Exp.Exp {
+export function readback(mod: Mod.Mod, ctx: Ctx.Ctx, normal: Normal.Normal): Exp.Exp {
   return Value.readback(mod, ctx, normal.t, normal.value)
 }
