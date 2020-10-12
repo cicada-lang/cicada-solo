f01d8d072d3d7fc49f5c5bab8fb521086f09e7f5
Xie Yuheng <xyheme@gmail.com>
2020-10-12 12:24:30 +0800

    - [lang3] exp-check.ts -- replace nested conditional with guard clauses

--- src/lang3/exp/exp-check.ts
+++ src/lang3/exp/exp-check.ts
@@ -12,21 +12,14 @@ import { check_by_infer } from "./exp-check-by-infer"
 
 export function check(ctx: Ctx.Ctx, exp: Exp.Exp, t: Value.Value): void {
   try {
-    if (t.kind === "Value.union") {
-      check_union_type(ctx, exp, t)
-    } else if (exp.kind === "Exp.fn") {
-      check_fn(ctx, exp, Value.is_pi(ctx, t))
-    } else if (exp.kind === "Exp.obj") {
-      check_obj(ctx, exp, Value.is_cls(ctx, t))
-    } else if (exp.kind === "Exp.same") {
-      check_same(ctx, exp, Value.is_equal(ctx, t))
-    } else if (exp.kind === "Exp.begin") {
-      check_begin(ctx, exp, t)
-    } else if (exp.kind === "Exp.quote") {
-      check_quote(ctx, exp, t)
-    } else {
-      check_by_infer(ctx, exp, t)
-    }
+    if (t.kind === "Value.union") return check_union_type(ctx, exp, t)
+    if (exp.kind === "Exp.fn") return check_fn(ctx, exp, Value.is_pi(ctx, t))
+    if (exp.kind === "Exp.obj") return check_obj(ctx, exp, Value.is_cls(ctx, t))
+    if (exp.kind === "Exp.same")
+      return check_same(ctx, exp, Value.is_equal(ctx, t))
+    if (exp.kind === "Exp.begin") return check_begin(ctx, exp, t)
+    if (exp.kind === "Exp.quote") return check_quote(ctx, exp, t)
+    return check_by_infer(ctx, exp, t)
   } catch (error) {
     if (error instanceof Trace.Trace) {
       throw Trace.trail(error, exp)
