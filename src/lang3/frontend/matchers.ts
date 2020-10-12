dcd9343aa674ab5767776a6a9397c834ed07bcc1
Xie Yuheng <xyheme@gmail.com>
2020-10-12 22:46:56 +0800

    - [lang3] frontend/matchers -- matcher for `Top`

--- src/lang3/frontend/matchers.ts
+++ src/lang3/frontend/matchers.ts
@@ -1,4 +1,5 @@
 import * as Exp from "../exp"
+import * as Top from "../top"
 import * as Stmt from "../stmt"
 import * as pt from "../../partech"
 import * as ut from "../../ut"
@@ -99,6 +100,34 @@ export function deduction_entry_matcher(
       t: exp_matcher(t),
       exp: exp_matcher(exp),
     }),
+  })(tree)
+}
+
+export function tops_matcher(tree: pt.Tree.Tree): Array<Top.Top> {
+  return pt.Tree.matcher<Array<Top.Top>>({
+    "tops:tops": ({ tops }) =>
+      pt.matchers.zero_or_more_matcher(tops).map(top_matcher),
+  })(tree)
+}
+
+export function top_matcher(tree: pt.Tree.Tree): Top.Top {
+  return pt.Tree.matcher<Top.Top>({
+    "top:def": ({ name, exp }) => Top.def(pt.Tree.str(name), exp_matcher(exp)),
+    "top:claim": ({ claim, t, define, exp }, { span }) => {
+      if (pt.Tree.str(claim) !== pt.Tree.str(define)) {
+        throw new pt.ParsingError(
+          "Name mismatch.\n" +
+            `- name to claim  : ${pt.Tree.str(claim)}\n` +
+            `- name to define : ${pt.Tree.str(define)}\n`,
+          { span }
+        )
+      }
+      return Top.def(
+        pt.Tree.str(claim),
+        Exp.the(exp_matcher(t), exp_matcher(exp))
+      )
+    },
+    "top:show": ({ exp }) => Top.show(exp_matcher(exp)),
   })(tree)
 }
 
