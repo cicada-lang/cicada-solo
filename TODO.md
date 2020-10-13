8633b94bfb4e2c8e718e26726363f36c75a02a3a
Xie Yuheng <xyheme@gmail.com>
2020-10-13 17:05:40 +0800

    - [lang3] extract function from loop `Value.readback_type_cls`

--- TODO.md
+++ TODO.md
@@ -30,8 +30,6 @@
 > 由于，重新理解了 Value Neutral Normal 的结构，
 > 并且简化了语言的功能，因此有很大机会重新做出一个成功的实现。
 
-- [lang3] extract function from loop `Value.readback_type_cls`
-
 - [lang3] add `opts.shadow_mod_p` to `Exp.evaluate`
 - [lang3] use `opts.shadow_mod_p` in `Exp.evaluate` of `Value.readback_cls` on scope
   - to implement:
--- src/lang3/value/value-readback-type-cls.ts
+++ src/lang3/value/value-readback-type-cls.ts
@@ -1,5 +1,6 @@
 import * as Value from "../value"
 import * as Closure from "../closure"
+import * as Telescope from "../telescope"
 import * as Neutral from "../neutral"
 import * as Exp from "../exp"
 import * as Ctx from "../ctx"
@@ -13,11 +14,18 @@ export function readback_type_cls(
   ctx: Ctx.Ctx,
   cls: Value.cls
 ): Exp.Exp {
-  const { sat, tel } = cls
-  const { next, scope } = tel
-  let { env } = cls.tel
-  env = Env.clone(env)
   ctx = Ctx.clone(ctx)
+  // NOTE side-effect on ctx
+  const norm_sat = readback_sat(mod, ctx, cls.sat)
+  const norm_scope = readback_scope(mod, ctx, cls.tel)
+  return Exp.cls(norm_sat, norm_scope)
+}
+
+function readback_sat(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  sat: Array<{ name: string; t: Value.Value; value: Value.Value }>
+): Array<{ name: string; t: Exp.Exp; exp: Exp.Exp }> {
   const norm_sat = new Array()
   for (const entry of sat) {
     const name = entry.name
@@ -26,16 +34,25 @@ export function readback_type_cls(
     norm_sat.push({ name, t, exp })
     Ctx.update(ctx, name, entry.t, entry.value)
   }
+  return norm_sat
+}
+
+function readback_scope(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  tel: Telescope.Telescope
+): Array<{ name: string; t: Exp.Exp }> {
   const norm_scope = new Array()
-  if (next !== undefined) {
-    const name = next.name
-    const t = Value.readback(mod, ctx, Value.type, next.t)
+  const env = Env.clone(tel.env)
+  if (tel.next !== undefined) {
+    const name = tel.next.name
+    const t = Value.readback(mod, ctx, Value.type, tel.next.t)
     norm_scope.push({ name, t })
-    Ctx.update(ctx, name, next.t)
-    Env.update(env, name, Value.not_yet(next.t, Neutral.v(name)))
+    Ctx.update(ctx, name, tel.next.t)
+    Env.update(env, name, Value.not_yet(tel.next.t, Neutral.v(name)))
   }
   // NOTE the `tel.mod` is used in the following, instead of `mod`
-  for (const entry of scope) {
+  for (const entry of tel.scope) {
     const name = entry.name
     const t_value = Exp.evaluate(tel.mod, env, entry.t)
     const t = Value.readback(tel.mod, ctx, Value.type, t_value)
@@ -43,5 +60,5 @@ export function readback_type_cls(
     Ctx.update(ctx, name, t_value)
     Env.update(env, name, Value.not_yet(t_value, Neutral.v(name)))
   }
-  return Exp.cls(norm_sat, norm_scope)
+  return norm_scope
 }
