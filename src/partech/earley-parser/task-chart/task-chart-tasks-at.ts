e5bc86ebe9d23faaf5cd2409ec3b35a01b20af1c
Xie Yuheng <xyheme@gmail.com>
2020-10-15 11:34:48 +0800

    up

--- src/partech/earley-parser/task-chart/task-chart-tasks-at.ts
+++ src/partech/earley-parser/task-chart/task-chart-tasks-at.ts
@@ -1,5 +1,6 @@
 import * as TaskChart from "../task-chart"
 import * as Task from "../task"
+import * as ut from "../../../ut"
 
 export function tasks_at(
   chart: TaskChart.TaskChart,
@@ -8,7 +9,11 @@ export function tasks_at(
   const task_map = chart[index]
 
   if (task_map === undefined) {
-    console.log(index, chart)
+    throw new Error(
+      "task_map is empty\n" +
+        `index: ${index}\n` +
+        `chart: ${ut.inspect(chart)}\n`
+    )
   }
 
   return task_map.values()
