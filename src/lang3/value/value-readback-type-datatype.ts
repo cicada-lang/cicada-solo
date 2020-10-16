4f4e0c834abd71955c66b27958602b0784f0cc32
Xie Yuheng <xyheme@gmail.com>
2020-10-16 18:05:31 +0800

    - [lang3] `Value.datatype` -- `readback`

---
+++ src/lang3/value/value-readback-type-datatype.ts
@@ -0,0 +1,35 @@
+import * as Value from "../value"
+import * as Neutral from "../neutral"
+import * as Exp from "../exp"
+import * as Ctx from "../ctx"
+import * as Env from "../env"
+import * as Mod from "../mod"
+
+export function readback_type_datatype(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  datatype: Value.datatype
+): Exp.Exp {
+  return Exp.datatype(
+    datatype.name,
+    Value.readback(mod, ctx, Value.type, datatype.t),
+    readback_delayed_sums(mod, ctx, datatype.delayed)
+  )
+}
+
+function readback_delayed_sums(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  delayed: Value.DelayedSums.DelayedSums
+): Array<{ tag: string; t: Exp.Exp }> {
+  // TODO should we use `mod` or `delayed.mod`?
+  return delayed.sums.map((sum) => ({
+    tag: sum.tag,
+    t: Value.readback(
+      mod,
+      ctx,
+      Value.type,
+      Exp.evaluate(delayed.mod, delayed.env, sum.t)
+    ),
+  }))
+}
