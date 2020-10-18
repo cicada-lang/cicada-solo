315a0deb25626f4be978931cc64d7b764bac5770
Xie Yuheng <xyheme@gmail.com>
2020-10-18 16:22:27 +0800

    up

--- src/lang3/exp/exp-check-fn.ts
+++ src/lang3/exp/exp-check-fn.ts
@@ -1,8 +1,10 @@
 import * as Exp from "../exp"
 import * as Value from "../value"
+import * as Pattern from "../pattern"
 import * as Neutral from "../neutral"
 import * as Ctx from "../ctx"
 import * as Mod from "../mod"
+import * as Trace from "../../trace"
 
 export function check_fn(
   mod: Mod.Mod,
@@ -10,7 +12,63 @@ export function check_fn(
   fn: Exp.fn,
   pi: Value.pi
 ): void {
-  const arg = Value.not_yet(pi.arg_t, Neutral.v(fn.name))
-  const ret_t = Value.Closure.apply(pi.ret_t_cl, arg)
-  Exp.check(mod, Ctx.extend(ctx, fn.name, pi.arg_t), fn.ret, ret_t)
+  const result_ctx = check_match(mod, ctx, fn.pattern, pi.arg_t)
+  if (result_ctx === undefined) throw new Trace.Trace("pattern mismatch")
+  Exp.check(
+    mod,
+    result_ctx,
+    fn.ret,
+    Value.Closure.apply(
+      pi.ret_t_cl,
+      Value.not_yet(pi.arg_t, Neutral.v(Value.pi_arg_name(pi)))
+    )
+  )
+}
+
+function check_match(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  pattern: Pattern.Pattern,
+  t: Value.Value
+): undefined | Ctx.Ctx {
+  return match_with(mod, ctx, pattern, t, new Map())
+}
+
+// NOTE side effect on `matched`
+function match_with(
+  mod: Mod.Mod,
+  ctx: Ctx.Ctx,
+  pattern: Pattern.Pattern,
+  t: Value.Value,
+  matched: Map<string, Value.Value>
+): undefined | Ctx.Ctx {
+  switch (pattern.kind) {
+    case "Pattern.v": {
+      const found = matched.get(pattern.name)
+      if (found === undefined)
+        return Ctx.extend(ctx, pattern.name, t)
+      if (Value.conversion(mod, ctx, Value.type, found, t))
+        return ctx
+      return undefined
+    }
+    case "Pattern.datatype": {
+      // if (
+      //   value.kind === "Value.datatype" &&
+      //   value.type_constructor.name === pattern.name
+      // ) {
+      //   return match_args_with(env, pattern.args, value.args, matched)
+      // }
+      return undefined
+    }
+    case "Pattern.data": {
+      // if (
+      //   value.kind === "Value.data" &&
+      //   value.data_constructor.type_constructor.name === pattern.name &&
+      //   value.data_constructor.tag === pattern.tag
+      // ) {
+      //   return match_args_with(env, pattern.args, value.args, matched)
+      // }
+      return undefined
+    }
+  }
 }
--- src/lang3/pattern/pattern-match.ts
+++ src/lang3/pattern/pattern-match.ts
@@ -11,22 +11,22 @@ export function match(
   return match_with(env, pattern, value, new Map())
 }
 
-// NOTE side effect on `matched_values`
+// NOTE side effect on `matched`
 function match_with(
   env: Env.Env,
   pattern: Pattern.Pattern,
   value: Value.Value,
-  matched_values: Map<string, Value.Value>
+  matched: Map<string, Value.Value>
 ): undefined | Env.Env {
   switch (pattern.kind) {
     case "Pattern.v": {
-      const matched_value = matched_values.get(pattern.name)
-      if (matched_value === undefined) {
-        matched_values.set(pattern.name, value)
+      const found = matched.get(pattern.name)
+      if (found === undefined) {
+        matched.set(pattern.name, value)
         return Env.extend(env, pattern.name, value)
       }
       // NOTE not `Value.conversion()` because we do not have `ctx`
-      if (ut.equal(matched_value, value)) return env
+      if (ut.equal(found, value)) return env
       return undefined
     }
     case "Pattern.datatype": {
@@ -34,7 +34,7 @@ function match_with(
         value.kind === "Value.datatype" &&
         value.type_constructor.name === pattern.name
       ) {
-        return match_args_with(env, pattern.args, value.args, matched_values)
+        return match_args_with(env, pattern.args, value.args, matched)
       }
       return undefined
     }
@@ -44,25 +44,26 @@ function match_with(
         value.data_constructor.type_constructor.name === pattern.name &&
         value.data_constructor.tag === pattern.tag
       ) {
-        return match_args_with(env, pattern.args, value.args, matched_values)
+        return match_args_with(env, pattern.args, value.args, matched)
       }
       return undefined
     }
   }
 }
 
+// NOTE side effect on `matched`
 function match_args_with(
   env: Env.Env,
   patterns: Array<Pattern.Pattern>,
   args: Array<Value.Value>,
-  matched_values: Map<string, Value.Value>
+  matched: Map<string, Value.Value>
 ): undefined | Env.Env {
   if (patterns.length !== args.length) return undefined
   let result: undefined | Env.Env = env
   for (let i = 0; i < patterns.length; i++) {
     const pattern = patterns[i]
     const arg = args[i]
-    result = match_with(result, pattern, arg, matched_values)
+    result = match_with(result, pattern, arg, matched)
     if (result === undefined) return undefined
   }
   return result
