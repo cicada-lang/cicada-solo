2b2f1a2c497058c0b1b1f54d9190994cf77abed1
Xie Yuheng <xyheme@gmail.com>
2020-10-22 17:49:19 +0800

    up

--- src/ut/in-browser-p.ts
+++ src/ut/in-browser-p.ts
@@ -1,3 +1,6 @@
+// NOTE
+// https://stackoverflow.com/questions/34550890/how-to-detect-if-script-is-running-in-browser-or-in-node-js
+
 export function in_browser_p(): boolean {
   return typeof window === "object"
 }
