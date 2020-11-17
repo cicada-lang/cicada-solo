01870f4c8c9b318e86e0f75a7911e0bb6d48577f
Xie Yuheng <xyheme@gmail.com>
2020-11-17 09:23:05 +0800

    - [lang3] refactor helper functions of `Top.run_tops`

--- src/lang3/top/top-run-tops.ts
+++ src/lang3/top/top-run-tops.ts
@@ -16,65 +16,65 @@ export function run_tops(
   mod: Mod.Mod,
   tops: Array<Top.Top>
 ): string {
-  define(project, mod, tops)
-  check(mod, tops)
-  return show(mod, tops)
-}
-
-function define(
-  project: Project.Project,
-  mod: Mod.Mod,
-  tops: Array<Top.Top>
-): void {
   for (const top of tops) {
-    if (top.kind === "Top.import") {
-      const imported_mod = Project.lookup_mod(project, top.modpath)
-      if (imported_mod === undefined)
-        throw new Error(`Unknown mod: ${Modpath.repr(top.modpath)}`)
-
-      Mod.update(mod, top.modpath.name, Mod.Den.mod(top.modpath, imported_mod))
-    }
-
-    if (top.kind === "Top.def") {
-      Mod.update(mod, top.name, Mod.Den.def(top.exp, top.t))
-    }
-
-    if (top.kind === "Top.type_constructor") {
-      Mod.update(
-        mod,
-        top.type_constructor.name,
-        Mod.Den.type_constructor(top.type_constructor)
-      )
-    }
+    Top.define(project, mod, top)
   }
-}
 
-function check(mod: Mod.Mod, tops: Array<Top.Top>): void {
   for (const top of tops) {
-    if (top.kind === "Top.def") {
-      const t = Mod.lookup_type(mod, top.name)!
-      const ctx = Ctx.extend(Ctx.init(), top.name, t)
-      Check.check(mod, ctx, top.exp, t)
-    }
-
-    if (top.kind === "Top.type_constructor") {
-      Infer.infer(mod, Ctx.init(), top.type_constructor)
-    }
+    Top.check(mod, top)
   }
-}
 
-function show(mod: Mod.Mod, tops: Array<Top.Top>): string {
   let output = ""
   for (const top of tops) {
-    if (top.kind === "Top.show") {
-      const env = Env.init()
-      const ctx = Ctx.init()
-      const t = Infer.infer(mod, ctx, top.exp)
-      const value = Evaluate.evaluate(mod, env, top.exp)
-      const value_repr = Exp.repr(Readback.readback(mod, ctx, t, value))
-      output += `${value_repr}\n`
-    }
+    output += Top.output(mod, top)
   }
 
   return output
+}
+
+export function define(project: Project.Project, mod: Mod.Mod, top: Top.Top): void {
+  if (top.kind === "Top.import") {
+    const imported_mod = Project.lookup_mod(project, top.modpath)
+    if (imported_mod === undefined)
+      throw new Error(`Unknown mod: ${Modpath.repr(top.modpath)}`)
+
+    Mod.update(mod, top.modpath.name, Mod.Den.mod(top.modpath, imported_mod))
+  }
+
+  if (top.kind === "Top.def") {
+    Mod.update(mod, top.name, Mod.Den.def(top.exp, top.t))
+  }
+
+  if (top.kind === "Top.type_constructor") {
+    Mod.update(
+      mod,
+      top.type_constructor.name,
+      Mod.Den.type_constructor(top.type_constructor)
+    )
+  }
+}
+
+export function check(mod: Mod.Mod, top: Top.Top): void {
+  if (top.kind === "Top.def") {
+    const t = Mod.lookup_type(mod, top.name)!
+    const ctx = Ctx.extend(Ctx.init(), top.name, t)
+    Check.check(mod, ctx, top.exp, t)
+  }
+
+  if (top.kind === "Top.type_constructor") {
+    Infer.infer(mod, Ctx.init(), top.type_constructor)
+  }
+}
+
+export function output(mod: Mod.Mod, top: Top.Top): string {
+  if (top.kind === "Top.show") {
+    const env = Env.init()
+    const ctx = Ctx.init()
+    const t = Infer.infer(mod, ctx, top.exp)
+    const value = Evaluate.evaluate(mod, env, top.exp)
+    const value_repr = Exp.repr(Readback.readback(mod, ctx, t, value))
+    return `${value_repr}\n`
+  }
+
+  return ""
 }
