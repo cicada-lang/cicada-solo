470795110d47bec9160890e06237e12b7e2abc2d
Xie Yuheng <xyheme@gmail.com>
2020-11-22 16:20:35 +0800

    up

--- src/play/final.ts
+++ src/play/final.ts
@@ -1,11 +1,28 @@
+import * as ut from "../ut"
+
 export type De<T> = {
   lit: (n: number) => T
   neg: (x: T) => T
   add: (x: T, y: T) => T
 }
 
+// NOTE This means we can have both initial and final encoding.
+// - just like `from_present` we can define `from_initial`.
+function from_present<T>(de: De<T>, present: ut.Json): T {
+  if (typeof present === "number") return de.lit(present)
+  present = ut.assert_json_object(present)
+  if (present["$lit"]) return de.lit(ut.assert_json_number(present["$lit"]))
+  if (present["$neg"]) return de.neg(from_present(de, present["$neg"]))
+  if (present["$add"]) {
+    const args = ut.assert_json_array(present["$add"])
+    return de.add(from_present(de, args[0]), from_present(de, args[1]))
+  }
+  throw new Error(`Unknown present: ${ut.inspect(present)}`)
+}
+
 function tf1<T>(de: De<T>): T {
-  return de.add(de.lit(8), de.neg(de.add(de.lit(1), de.lit(2))))
+  // return de.add(de.lit(8), de.neg(de.add(de.lit(1), de.lit(2))))
+  return from_present(de, { $add: [8, { $neg: { $add: [1, 2] } }] })
 }
 
 const number_de: De<number> = {
