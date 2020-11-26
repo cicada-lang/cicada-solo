c7207ef594505b1422c4345b95c1ed162f666159
Xie Yuheng <xyheme@gmail.com>
2020-11-26 20:31:24 +0800

    up

--- src/final/lang0.ts
+++ src/final/lang0.ts
@@ -1,7 +1,25 @@
-import { $, _ } from "../hkts"
+export type De<T> = {
+  v: (name: string) => T
+  fn: (name: string, ret: T) => T
+  ap: (target: T, arg: T) => T
+}
 
-export type Lang0<Exp> = {
-  v: Exp
-  fn: (name: string, ret: Exp) => Exp
-  ap: (target: Exp, arg: Exp) => Exp
+export type Value = (arg: Value) => Value
+
+export type Env = Map<string, Value>
+
+function extend(env: Env, name: string, value: Value): Env {
+  const new_env = new Map(env)
+  new_env.set(name, value)
+  return new_env
+}
+
+const de_value: De<(env: Env) => Value> = {
+  v: (name) => (env) => {
+    const value = env.get(name)
+    if (!value) throw new Error(`Unknown name: ${name}`)
+    return value
+  },
+  fn: (name, ret) => (env) => (arg) => ret(extend(env, name, arg)),
+  ap: (target, arg) => (env) => target(env)(arg(env)),
 }
