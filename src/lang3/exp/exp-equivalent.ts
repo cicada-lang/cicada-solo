5b5cc1e790fa834c701cfa04246014b553410d7d
Xie Yuheng <xyheme@gmail.com>
2020-12-13 22:45:02 +0800

    up

--- src/lang3/exp/exp-equivalent.ts
+++ src/lang3/exp/exp-equivalent.ts
@@ -1,6 +1,6 @@
-import * as Exp from "../exp"
+import { Exp } from "../exp"
 
-export function equivalent(x: Exp.Exp, y: Exp.Exp): boolean {
+export function equivalent(x: Exp, y: Exp): boolean {
   const ctx = { depth: 0, depths: new Map() }
   return x.alpha_repr(ctx) === y.alpha_repr(ctx)
 }
