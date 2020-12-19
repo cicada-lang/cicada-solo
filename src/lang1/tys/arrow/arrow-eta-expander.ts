7f423c8a04b9badf04b02e8224f66b184c086804
Xie Yuheng <xyheme@gmail.com>
2020-12-19 23:14:55 +0800

    up

--- src/lang1/tys/arrow/arrow-eta-expander.ts
+++ src/lang1/tys/arrow/arrow-eta-expander.ts
@@ -7,12 +7,13 @@ import { readback } from "../../readback"
 import { do_ap } from "../../exps/ap"
 import * as ut from "../../../ut"
 
+// NOTE everything with a function type
+//   is immediately read back as having a Lambda on top.
+//   This implements the η-rule for functions.
+
 export const arrow_eta_expander = (arg_t: Ty, ret_t: Ty) =>
   EtaExpander({
     eta_expand: (value, { used }) => {
-      // NOTE everything with a function type
-      //   is immediately read back as having a Lambda on top.
-      //   This implements the η-rule for functions.
       const name = ut.freshen_name(used, value_arg_name(value))
       const variable = Value.not_yet(arg_t, Neutral.v(name))
       const ret = do_ap(value, variable)
