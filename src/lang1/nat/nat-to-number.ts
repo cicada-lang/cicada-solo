1bc03286e4fd77969e4135064da277a1b309ecfa
Xie Yuheng <xyheme@gmail.com>
2020-12-19 19:34:20 +0800

    up

--- src/lang1/nat/nat-to-number.ts
+++ src/lang1/nat/nat-to-number.ts
@@ -5,7 +5,7 @@ export function nat_to_number(exp: Exp.Exp): number | undefined {
   if (exp.kind === "Exp.zero") {
     return 0
   } else if (exp.kind === "Exp.add1") {
-    const add1 = (exp as Exp.add1)
+    const add1 = exp as Exp.add1
     const almost = nat_to_number(add1.prev)
     if (almost !== undefined) {
       return 1 + almost
