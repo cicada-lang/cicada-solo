20cd31b2d28be398a1f407a5a153d6ca87bd6573
Xie Yuheng <xyheme@gmail.com>
2020-12-20 17:31:22 +0800

    up

--- src/lang1/exps/add1/add1-evaluable.ts
+++ src/lang1/exps/add1/add1-evaluable.ts
@@ -1,7 +1,7 @@
 import { Evaluable } from "../../evaluable"
 import { evaluate } from "../../evaluate"
 import { Exp } from "../../exp"
-import {Add1Value} from "../../exps/add1-value"
+import { Add1Value } from "../../exps/add1-value"
 
 export const add1_evaluable = (prev: Exp) =>
   Evaluable({
