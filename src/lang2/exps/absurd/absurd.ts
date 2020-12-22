fce93136b752842bd325059f4c3d32bafc3c8811
Xie Yuheng <xyheme@gmail.com>
2020-12-22 10:49:19 +0800

    - [lang2] `AlphaRepr` -- absurd

--- src/lang2/exps/absurd/absurd.ts
+++ src/lang2/exps/absurd/absurd.ts
@@ -2,9 +2,10 @@ import { Exp } from "../../exp"
 import { Evaluable } from "../../evaluable"
 import { Repr } from "../../repr"
 import * as Value from "../../value"
+import { AlphaRepr } from "../../alpha-repr"
 
 export type Absurd = Evaluable &
-  Repr & {
+  Repr & AlphaRepr & {
     kind: "Exp.absurd"
   }
 
@@ -12,4 +13,5 @@ export const Absurd: Absurd = {
   kind: "Exp.absurd",
   evaluability: (_) => Value.absurd,
   repr: () => "Absurd",
+  alpha_repr: (opts) => "Absurd",
 }
