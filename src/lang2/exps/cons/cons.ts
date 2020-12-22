f410ea84d8f07bf05ef861bf60043aa84152dca1
Xie Yuheng <xyheme@gmail.com>
2020-12-22 10:59:21 +0800

    - [lang2] `AlphaRepr` -- cons

--- src/lang2/exps/cons/cons.ts
+++ src/lang2/exps/cons/cons.ts
@@ -2,9 +2,11 @@ import { Exp } from "../../exp"
 import { Evaluable } from "../../evaluable"
 import { cons_evaluable } from "./cons-evaluable"
 import { Repr } from "../../repr"
+import { AlphaRepr } from "../../alpha-repr"
 
 export type Cons = Evaluable &
-  Repr & {
+  Repr &
+  AlphaRepr & {
     kind: "Exp.cons"
     car: Exp
     cdr: Exp
@@ -15,7 +17,8 @@ export function Cons(car: Exp, cdr: Exp): Cons {
     kind: "Exp.cons",
     car,
     cdr,
-    repr: () => `cons(${car.repr()}, ${cdr.repr()})`,
     ...cons_evaluable(car, cdr),
+    alpha_repr: (opts) =>
+      `cons(${car.alpha_repr(opts)}, ${cdr.alpha_repr(opts)})`,
   }
 }
