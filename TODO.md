08d8ad887867acbd3bd29789fcf4315407ff3a55
Xie Yuheng <xyheme@gmail.com>
2020-12-22 21:47:08 +0800

    [lang0] `Evaluable` -- `Begin`

--- TODO.md
+++ TODO.md
@@ -43,8 +43,6 @@
 
 > refactoring `Exp`
 
-- [lang0] `Evaluable` -- `Begin`
-
 - [lang0] no branches in `evaluate`
 
 - [lang0] `Repr`
--- src/lang0/exps/begin/begin.ts
+++ src/lang0/exps/begin/begin.ts
@@ -1,7 +1,11 @@
 import { Exp } from "../../exp"
 import * as Stmt from "../../stmt"
+import { Evaluable } from "../../evaluable"
+import { evaluate } from "../../evaluate"
+import * as Value from "../../value"
+import * as Env from "../../env"
 
-export type Begin = {
+export type Begin = Evaluable & {
   kind: "Exp.begin"
   stmts: Array<Stmt.Stmt>
   ret: Exp
@@ -12,5 +16,14 @@ export function Begin(stmts: Array<Stmt.Stmt>, ret: Exp): Begin {
     kind: "Exp.begin",
     stmts,
     ret,
+    ...Evaluable({
+      evaluability: ({ env }) => {
+        const new_env = Env.clone(env)
+        for (const stmt of stmts) {
+          Stmt.execute(new_env, stmt)
+        }
+        return evaluate(new_env, ret)
+      }
+    }),
   }
 }
