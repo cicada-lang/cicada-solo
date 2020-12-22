feb2d50534fecd200a3c0d11662cad2a6cba2a8d
Xie Yuheng <xyheme@gmail.com>
2020-12-22 10:55:51 +0800

    - [lang2] `AlphaRepr` -- add1

--- src/lang2/exps/add1/add1.ts
+++ src/lang2/exps/add1/add1.ts
@@ -3,11 +3,11 @@ import { Evaluable } from "../../evaluable"
 import { evaluate } from "../../evaluate"
 import * as Value from "../../value"
 import { Repr } from "../../repr"
-
+import { AlphaRepr } from "../../alpha-repr"
 import { nat_to_number } from "../../exp"
 
 export type Add1 = Evaluable &
-  Repr & {
+  Repr & AlphaRepr & {
     kind: "Exp.add1"
     prev: Exp
   }
@@ -16,6 +16,7 @@ export function Add1(prev: Exp): Add1 {
   return {
     kind: "Exp.add1",
     prev,
+    evaluability: ({ env }) => Value.add1(evaluate(env, prev)),
     repr() {
       const n = nat_to_number(this)
       if (n !== undefined) {
@@ -24,6 +25,13 @@ export function Add1(prev: Exp): Add1 {
         return `add1(${prev.repr()})`
       }
     },
-    evaluability: ({ env }) => Value.add1(evaluate(env, prev)),
+    alpha_repr(opts) {
+      const n = nat_to_number(this)
+      if (n !== undefined) {
+        return n.toString()
+      } else {
+        return `add1(${prev.alpha_repr()})`
+      }
+    },
   }
 }
