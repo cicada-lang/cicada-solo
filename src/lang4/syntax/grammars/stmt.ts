96d707cbb5d535fcb5145bdd0e2c8cd5100150d5
Xie Yuheng <xyheme@gmail.com>
2020-12-26 16:33:35 +0800

    up

--- src/lang4/syntax/grammars/stmt.ts
+++ src/lang4/syntax/grammars/stmt.ts
@@ -12,3 +12,9 @@ export const stmt = {
     "stmt:show": ["@show", { jojo: "jojo" }],
   },
 }
+
+export const stmts = {
+  $grammar: {
+    "stmts:stmts": [{ stmts: { $ap: ["zero_or_more", "stmt"] } }],
+  },
+}
