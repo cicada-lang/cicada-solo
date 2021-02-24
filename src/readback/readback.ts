ce18dd7cb09c46ee1e22e546ecbdf7c4d73875af
Xie Yuheng <xyheme@gmail.com>
2021-02-25 02:24:48 +0800

    up

--- src/readback/readback.ts
+++ src/readback/readback.ts
@@ -27,9 +27,13 @@ export function readback(
 ): Exp.Exp {
   if (t.kind === "Value.nat" && value.kind === "Value.zero") {
     return new Zero()
-  } else if (t.kind === "Value.nat" && value.kind === "Value.add1") {
+  }
+
+  if (t.kind === "Value.nat" && value.kind === "Value.add1") {
     return new Add1(Readback.readback(ctx, t, value.prev))
-  } else if (t.kind === "Value.pi") {
+  }
+
+  if (t.kind === "Value.pi") {
     // NOTE everything with a function type
     //   is immediately read back as having a Lambda on top.
     //   This implements the η-rule for functions.
@@ -43,7 +47,9 @@ export function readback(
         do_ap(value, variable)
       )
     )
-  } else if (t.kind === "Value.sigma") {
+  }
+
+  if (t.kind === "Value.sigma") {
     // NOTE Pairs are also η-expanded.
     //   Every value with a pair type,
     //   whether it is neutral or not,
@@ -54,36 +60,56 @@ export function readback(
       Readback.readback(ctx, t.car_t, car),
       Readback.readback(ctx, Value.Closure.apply(t.cdr_t_cl, car), cdr)
     )
-  } else if (t.kind === "Value.trivial") {
+  }
+
+  if (t.kind === "Value.trivial") {
     // NOTE the η-rule for trivial states that
     //   all of its inhabitants are the same as sole.
     //   This is implemented by reading the all back as sole.
     return new Sole()
-  } else if (
+  }
+
+  if (
     t.kind === "Value.absurd" &&
     value.kind === "Value.not_yet" &&
     value.t.kind === "Value.absurd"
   ) {
     return new The(new Absurd(), Readback.readback_neutral(ctx, value.neutral))
-  } else if (t.kind === "Value.equal" && value.kind === "Value.same") {
+  }
+
+  if (t.kind === "Value.equal" && value.kind === "Value.same") {
     return new Same()
-  } else if (t.kind === "Value.str" && value.kind === "Value.quote") {
+  }
+
+  if (t.kind === "Value.str" && value.kind === "Value.quote") {
     return new Quote(value.str)
-  } else if (t.kind === "Value.type" && value.kind === "Value.nat") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.nat") {
     return new Nat()
-  } else if (t.kind === "Value.type" && value.kind === "Value.str") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.str") {
     return new Str()
-  } else if (t.kind === "Value.type" && value.kind === "Value.trivial") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.trivial") {
     return new Trivial()
-  } else if (t.kind === "Value.type" && value.kind === "Value.absurd") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.absurd") {
     return new Absurd()
-  } else if (t.kind === "Value.type" && value.kind === "Value.equal") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.equal") {
     return new Equal(
       Readback.readback(ctx, Value.type, value.t),
       Readback.readback(ctx, value.t, value.from),
       Readback.readback(ctx, value.t, value.to)
     )
-  } else if (t.kind === "Value.type" && value.kind === "Value.sigma") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.sigma") {
     const fresh_name = ut.freshen_name(
       new Set(ctx.names()),
       value.cdr_t_cl.name
@@ -96,7 +122,9 @@ export function readback(
       Value.Closure.apply(value.cdr_t_cl, variable)
     )
     return new Sigma(fresh_name, car_t, cdr_t)
-  } else if (t.kind === "Value.type" && value.kind === "Value.pi") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.pi") {
     const fresh_name = ut.freshen_name(
       new Set(ctx.names()),
       value.ret_t_cl.name
@@ -109,18 +137,22 @@ export function readback(
       Value.Closure.apply(value.ret_t_cl, variable)
     )
     return new Pi(fresh_name, arg_t, ret_t)
-  } else if (t.kind === "Value.type" && value.kind === "Value.type") {
+  }
+
+  if (t.kind === "Value.type" && value.kind === "Value.type") {
     return new Type()
-  } else if (value.kind === "Value.not_yet") {
+  }
+
+  if (value.kind === "Value.not_yet") {
     // NOTE  t and value.t are ignored here,
     //  maybe use them to debug.
     return Readback.readback_neutral(ctx, value.neutral)
-  } else {
-    throw new Error(
-      ut.aline(`
+  }
+
+  throw new Error(
+    ut.aline(`
       |I can not readback value: ${ut.inspect(value)},
       |of type: ${ut.inspect(t)}.
       |`)
-    )
-  }
+  )
 }
