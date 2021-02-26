fddb80c562b1bc670d0dcf3326dba31766572eff
Xie Yuheng <xyheme@gmail.com>
2021-02-26 14:33:49 +0800

    up

--- src/readback/readback.ts
+++ src/readback/readback.ts
@@ -1,33 +1,14 @@
-import * as Readback from "../readback"
-import * as Evaluate from "../evaluate"
-import * as Value from "../value"
-import * as Neutral from "../neutral"
-import * as Exp from "../exp"
-import * as Ctx from "../ctx"
+import { readback_neutral } from "../readback"
+import { Value } from "../value"
+import { Exp } from "../exp"
+import { Ctx } from "../ctx"
+import { Absurd } from "../core"
+import { The } from "../core"
+import { AbsurdValue } from "../core"
+import { NotYetValue } from "../core"
 import * as ut from "../ut"
 
-import { Var } from "../core"
-import { Pi, Fn } from "../core"
-import { Sigma, Cons } from "../core"
-import { Nat, Zero, Add1 } from "../core"
-import { Equal, Same } from "../core"
-import { Absurd } from "../core"
-import { Trivial, Sole } from "../core"
-import { Str, Quote } from "../core"
-import { Type } from "../core"
-import { Let } from "../core"
-import { The } from "../core"
-import { PiValue } from "../core/pi-value"
-import { SigmaValue } from "../core/sigma-value"
-import { AbsurdValue } from "../core/absurd-value"
-import { TrivialValue } from "../core/trivial-value"
-import { NotYetValue } from "../core/not-yet-value"
-
-export function readback(
-  ctx: Ctx.Ctx,
-  t: Value.Value,
-  value: Value.Value
-): Exp.Exp {
+export function readback(ctx: Ctx, t: Value, value: Value): Exp {
   if (t.eta_expand) {
     return t.eta_expand(ctx, value)
   }
@@ -37,7 +18,7 @@ export function readback(
     value instanceof NotYetValue &&
     value.t instanceof AbsurdValue
   ) {
-    return new The(new Absurd(), Readback.readback_neutral(ctx, value.neutral))
+    return new The(new Absurd(), readback_neutral(ctx, value.neutral))
   }
 
   const exp = value.readback(ctx, t)
