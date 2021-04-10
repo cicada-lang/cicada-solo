8ae819808ecf7dd79cdd7efa5c73d0974d2cdb03
Xie Yuheng <xyheme@gmail.com>
2021-04-11 02:39:46 +0800

    - test without output

--- src/library/local-library.test.ts
+++ src/library/local-library.test.ts
@@ -1,10 +1,13 @@
 import { LocalLibrary } from "../library"
 import Path from "path"
+import * as ut from "../ut"
 
 async function test(): Promise<void> {
   const file = "../../libraries/algebra/library.json"
   const lib = await LocalLibrary.fromConfigFile(Path.resolve(__dirname, file))
-  console.log(lib)
+
+  const base_dir = "../../libraries/algebra"
+  ut.assert_equal(lib.base_dir, Path.resolve(__dirname, base_dir))
 }
 
 test().catch((error) => {
