1e523cc8fe6640d021b7247a95c6cfef1ed661d0
Xie Yuheng <xyheme@gmail.com>
2021-04-24 15:33:30 +0800

    - `Li.infer`

--- src/core/list/li.ts
+++ src/core/list/li.ts
@@ -3,6 +3,7 @@ import { Ctx } from "../../ctx"
 import { Env } from "../../env"
 import { evaluate } from "../../evaluate"
 import { check } from "../../check"
+import { infer } from "../../infer"
 import { Value } from "../../value"
 import { ListValue, LiValue } from "../../core"
 
@@ -19,8 +20,10 @@ export class Li implements Exp {
     return new LiValue(evaluate(env, this.head), evaluate(env, this.tail))
   }
 
-  check(ctx: Ctx, t: Value): void {
-    throw new Error("TODO")
+  infer(ctx: Ctx): Value {
+    const elem_t = infer(ctx, this.head)
+    check(ctx, this.tail, new ListValue(elem_t))
+    return new ListValue(elem_t)
   }
 
   repr(): string {
