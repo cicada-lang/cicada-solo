fd8a238497ce096b8829c0bd3e83c55c23fb08fb
Xie Yuheng <xyheme@gmail.com>
2021-05-02 03:59:06 +0800

    up

--- src/exps/pi/fn.ts
+++ src/exps/pi/fn.ts
@@ -22,7 +22,8 @@ export class Fn extends Exp {
     const pi = expect(ctx, t, Cores.PiValue)
     const arg = new Cores.NotYetValue(pi.arg_t, new Cores.VarNeutral(this.name))
     const ret_t = pi.ret_t_cl.apply(arg)
-    check(ctx.extend(this.name, pi.arg_t), this.ret, ret_t)
+    const ret_core = check(ctx.extend(this.name, pi.arg_t), this.ret, ret_t)
+    return new Cores.Fn(this.name, ret_core)
   }
 
   repr(): string {
