c0ae8a14ca6b962721c41f40485df1cd4316d8ee
Xie Yuheng <xyheme@gmail.com>
2021-05-02 23:56:42 +0800

    up

--- src/cores/nat/nat-util.ts
+++ src/cores/nat/nat-util.ts
@@ -1,15 +1,6 @@
 import { Core } from "../../core"
 import * as Cores from "../../cores"
 
-export function nat_from_number(n: number): Core {
-  if (n <= 0) {
-    return new Cores.Zero()
-  } else {
-    const almost = nat_from_number(n - 1)
-    return new Cores.Add1(almost)
-  }
-}
-
 export function nat_to_number(exp: Core): number | undefined {
   if (exp instanceof Cores.Zero) {
     return 0
