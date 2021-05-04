09bed3373d43cc4d420edad138e008d96ebc905a
Xie Yuheng <xyheme@gmail.com>
2021-05-05 04:24:19 +0800

    up

--- libraries/add-remotes.sh
+++ libraries/add-remotes.sh
@@ -1,2 +1,2 @@
-git remote add libraries/cicada-stdlib git@github.com:cicada-lang
+git remote add libraries/cicada-stdlib git@github.com:cicada-lang/cicada-stdlib
 git remote add libraries/the-little-typer git@github.com:xieyuheng/the-little-typer
