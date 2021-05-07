c8720036aae5dc007f6064c57d964924736f1c94
Xie Yuheng <xyheme@gmail.com>
2021-05-07 21:10:53 +0800

    improve expect error report

--- src/expect/expect.ts
+++ src/expect/expect.ts
@@ -13,17 +13,17 @@ export function expect<T>(ctx: Ctx, value: Value, TheClass: Class<T>): T {
     try {
       const exp = readback(ctx, new Cores.TypeValue(), value)
       const message =
-        `I see unexpected value: ${exp.repr()}\n` +
-        `The asserted class is: ${TheClass.name}.\n`
+        `I see unexpected value class: ${value.constructor.name},\n` +
+        `which reads back to exp: ${exp.repr()},\n` +
+        `but the asserted class is: ${TheClass.name}.\n`
       throw new Trace(message)
     } catch (error) {
       // NOTE If the error makes us
       //   not be able to readback the value as `TypeValue`,
       //   we report error without `exp.repr()`.
       const message =
-        `I expect the value to be a Type\n` +
-        `But I see unexpected value class: ${value.constructor.name},\n` +
-        `The asserted class is: ${TheClass.name}.\n`
+        `I see unexpected value class: ${value.constructor.name},\n` +
+        `but the asserted class is: ${TheClass.name}.\n`
       throw new Trace(message)
     }
   }
