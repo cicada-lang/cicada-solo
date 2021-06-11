b86819a2c12220cc2bbd8a71921bfc9b3708aaa5
Xie Yuheng <xyheme@gmail.com>
2021-06-11 20:20:22 +0800

    up

--- src/conversion/conversion.ts
+++ src/conversion/conversion.ts
@@ -12,28 +12,9 @@ export function conversion(ctx: Ctx, t: Value, x: Value, y: Value): boolean {
 
 function alpha_equivalent(ctx: Ctx, x: Core, y: Core): boolean {
   const names = ctx.names()
-  // const alpha_ctx = names.reduce(
-  //   (alpha_ctx, name) => alpha_ctx.extend(name),
-  //   new AlphaCtx()
-  // )
   const alpha_ctx = new AlphaCtx()
   const x_repr = x.alpha_repr(alpha_ctx)
   const y_repr = y.alpha_repr(alpha_ctx)
-
-  // // TODO
-  // if (
-  //   names[names.length - 1] === "equal" &&
-  //   names[names.length - 2] === "j" &&
-  //   names[names.length - 3] === "almost"
-  // ) {
-  //   if (x_repr !== y_repr) {
-  //     console.log("(x_repr !== y_repr)")
-  //   }
-  //   console.log("names --", ctx.names())
-  //   console.log("x --", x_repr, x)
-  //   console.log("y --", y_repr, y)
-  // }
-
   return x_repr === y_repr
 }
 
