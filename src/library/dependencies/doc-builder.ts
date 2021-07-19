caa42043e9f635bdf57e6a87b5edd501bda9ff69
Xie Yuheng <xyheme@gmail.com>
2021-07-19 17:53:51 +0800

    up

--- src/library/dependencies/doc-builder.ts
+++ src/library/dependencies/doc-builder.ts
@@ -2,10 +2,10 @@ import { Doc } from "../../doc"
 import { Library } from "../../library"
 
 export interface DocBuilder<Module> {
-  right_extension_p: (path: string) => boolean
-  from_file: (opts: {
+  right_extension_p(path: string): boolean
+  from_file(opts: {
     path: string
     text: string
     library: Library<Module>
-  }) => Doc<Module>
+  }): Doc<Module>
 }
