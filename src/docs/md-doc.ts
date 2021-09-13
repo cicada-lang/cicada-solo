e33e83895206c4e0ce22e64280926d7c583e5a8a
Xie Yuheng <xyheme@gmail.com>
2021-09-14 05:00:46 +0800

    up

--- src/docs/md-doc.ts
+++ src/docs/md-doc.ts
@@ -20,47 +20,47 @@ export class MdDoc extends Doc {
       library,
       path: this.path,
       text,
-      stmts: this.code_blocks(text).flatMap((code_block) =>
+      stmts: code_blocks(text).flatMap((code_block) =>
         Syntax.parse_stmts(code_block.text, code_block.offset)
       ),
     })
   }
+}
 
-  private code_blocks(text: string): Array<{
-    info: string
-    text: string
-    offset: number
-  }> {
-    const reader = new commonmark.Parser()
-    const writer = new commonmark.HtmlRenderer()
-    const parsed: commonmark.Node = reader.parse(text)
+function code_blocks(text: string): Array<{
+  info: string
+  text: string
+  offset: number
+}> {
+  const reader = new commonmark.Parser()
+  const writer = new commonmark.HtmlRenderer()
+  const parsed: commonmark.Node = reader.parse(text)
 
-    const code_blocks = []
+  const code_blocks = []
 
-    const walker = parsed.walker()
+  const walker = parsed.walker()
 
-    let event, node
-    while ((event = walker.next())) {
-      node = event.node
+  let event, node
+  while ((event = walker.next())) {
+    node = event.node
 
-      if (event.entering && node.type === "code_block") {
-        if (node.literal && node.info === "cicada") {
-          const [start_pos, _end_pos] = node.sourcepos
-          const [row, col] = start_pos
-          code_blocks.push({
-            info: node.info,
-            text: node.literal,
-            offset: this.offset_from_pos(text, row, col),
-          })
-        }
+    if (event.entering && node.type === "code_block") {
+      if (node.literal && node.info === "cicada") {
+        const [start_pos, _end_pos] = node.sourcepos
+        const [row, col] = start_pos
+        code_blocks.push({
+          info: node.info,
+          text: node.literal,
+          offset: offset_from_pos(text, row, col),
+        })
       }
     }
-
-    return code_blocks
   }
 
-  private offset_from_pos(text: string, row: number, col: number): number {
-    const lines = text.split("\n")
-    return lines.slice(0, row).join("\n").length + col
-  }
+  return code_blocks
+}
+
+function offset_from_pos(text: string, row: number, col: number): number {
+  const lines = text.split("\n")
+  return lines.slice(0, row).join("\n").length + col
 }
