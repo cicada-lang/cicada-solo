703a9536dcdba7baf018661763f060ef237350b3
Xie Yuheng <xyheme@gmail.com>
2021-09-14 05:55:15 +0800

    up

--- src/module/module-loader.ts
+++ src/module/module-loader.ts
@@ -1,8 +1,6 @@
 import { Library } from "../library"
 import { Module } from "../module"
 
-// NOTE The responsibility of this class
-//   is to parse file to different kinds of doc.
 export abstract class ModuleLoader {
   abstract load(library: Library, path: string): Promise<Module>
 
