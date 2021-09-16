c9fd998a99675739a414bcb45436c231a40a5b47
Xie Yuheng <xyheme@gmail.com>
2021-09-16 22:54:26 +0800

    up

--- src/cli/repl.ts
+++ src/cli/repl.ts
@@ -3,41 +3,57 @@ import pt from "@cicada-lang/partech"
 const pkg = require("../../package.json")
 
 export class Repl {
-  async run(): Promise<void> {
-    const rl = Readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    })
+  rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
 
-    let lines: Array<string> = []
+  lines: Array<string> = []
 
+  constructor() {
+    this.listen()
+  }
+
+  greeting(): void {
     console.log(`Welcome to Cicada ${pkg.version} ^-^/`)
+  }
 
-    rl.prompt()
-    rl.on("line", (line) => {
-      const text = [...lines, line].join("\n")
+  get text(): string {
+    return this.lines.join("\n")
+  }
+
+  prompt(): void {
+    const depth = pt.lexers.common.parens_depth(this.text)
+    this.rl.setPrompt(createPrompt(depth))
+    this.rl.prompt()
+  }
+
+  run(): void {
+    this.greeting()
+    this.prompt()
+  }
+
+  private listen(): void {
+    this.rl.on("line", (line) => {
+      const text = this.text + "\n" + line + "\n"
       const result = pt.lexers.common.parens_check(text)
       if (result.kind === "lack") {
         // NOTE save
-        lines.push(line)
+        this.lines.push(line)
       } else if (result.kind === "balance") {
         // NOTE commit
         console.log(text)
-        lines = []
+        this.lines = []
       } else {
         // NOTE report error
         const report = pt.report(result.token.span, text)
         console.error()
         console.error(`Parentheses error: ${result.kind}`)
         console.error(report)
-        lines = []
+        this.lines = []
       }
 
-      {
-        const depth = pt.lexers.common.parens_depth(lines.join("\n"))
-        rl.setPrompt(createPrompt(depth))
-        rl.prompt()
-      }
+      this.prompt()
     })
   }
 }
