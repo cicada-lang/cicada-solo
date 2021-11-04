1ad936921d579a3f8865287762fdd5fcbae3a81f
Xie Yuheng <xyheme@gmail.com>
2021-11-04 23:17:54 +0800

    up

--- src/lang/parser/tests/equal.snapshot.ts
+++ src/lang/parser/tests/equal.snapshot.ts
@@ -5,6 +5,5 @@ tester.echo_exp(`Equal(t, from, to)`)
 tester.echo_exp(`refl`)
 
 tester.echo_exp(`same(x)`)
-tester.not_exp(`same`)
 
 tester.echo_exp(`replace(target, motive, base)`)
--- src/lang/parser/tests/var.snapshot.ts
+++ src/lang/parser/tests/var.snapshot.ts
@@ -2,8 +2,6 @@ import { tester } from "../parser-tester-instance"
 
 // NOTE preserved keywords
 
-tester.not_exp(`implicit`)
-
 tester.echo_exp(`123`)
 tester.echo_exp(`"abc"`)
 tester.echo_exp(`x`)
