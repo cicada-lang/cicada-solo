537ba1335aea4583417a7d855dcae337f8fda143
Xie Yuheng <xyheme@gmail.com>
2021-11-20 02:10:46 +0800

    - `check` use `check_by_infer`

--- src/lang/exp/check.ts
+++ src/lang/exp/check.ts
@@ -17,29 +17,7 @@ export function check(
     if (exp.check) {
       return exp.check(ctx, t, opts)
     } else if (exp.infer) {
-      const inferred = exp.infer(ctx, opts)
-      const u = inferred.t
-      if (!conversion(ctx, new Exps.TypeValue(), t, u)) {
-        const u_exp = readback(ctx, new Exps.TypeValue(), u)
-        const t_exp = readback(ctx, new Exps.TypeValue(), t)
-
-        // DEBUG
-        // console.log("inferred:", u_exp.format())
-        // console.dir(u, { depth: 4 })
-        // console.log("expected:", t_exp.format())
-        // console.dir(t, { depth: 4 })
-
-        throw new ExpTrace(
-          [
-            `I infer the type to be:`,
-            `  ${u_exp.format()}`,
-            `But the expected type is:`,
-            `  ${t_exp.format()}`,
-          ].join("\n")
-        )
-      }
-
-      return inferred.core
+      return check_by_infer(ctx, exp as ExpWithInfer, t, opts)
     } else {
       throw new ExpTrace(
         [
@@ -56,11 +34,13 @@ export function check(
   }
 }
 
+type ExpWithInfer = Exp & {
+  infer(ctx: Ctx, opts?: ElaborationOptions): { t: Value; core: Core }
+}
+
 export function check_by_infer(
   ctx: Ctx,
-  exp: Exp & {
-    infer(ctx: Ctx, opts?: ElaborationOptions): { t: Value; core: Core }
-  },
+  exp: ExpWithInfer,
   t: Value,
   opts?: ElaborationOptions
 ): Core {
