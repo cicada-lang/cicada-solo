e77168bb14fb580f70b332f84a6d4ffb91d14036
Xie Yuheng <xyheme@gmail.com>
2021-11-27 04:05:39 +0800

    - `returned-[pi|fn|ap]` -- syntax test

--- src/lang/parser/tests/returned-pi.snapshot.ts
+++ src/lang/parser/tests/returned-pi.snapshot.ts
@@ -2,9 +2,9 @@ import { tester } from "../parser-tester-instance"
 
 tester.echo_stmts(`
 
-(returned A: Type, head: A, tail: List(A)) => List(A)
+(returned A: Type, head: A, tail: List(A)) -> List(A)
 
-(returned A: Type) => A
-(returned A: Type, returned B: Type) => Pair(A, B)
+(returned A: Type) -> A
+(returned A: Type, returned B: Type) -> Pair(A, B)
 
 `)
