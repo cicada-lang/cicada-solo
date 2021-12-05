b0a194c55169df28321742db083c742e5ee58323
Xie Yuheng <xyheme@gmail.com>
2021-12-05 16:29:46 +0800

    - [refactor] `ListIndCore.apply` -- guard clauses instead of nested conditional

--- src/lang/exps/list/list-ind-core.ts
+++ src/lang/exps/list/list-ind-core.ts
@@ -54,39 +54,39 @@ export class ListIndCore extends Core {
   static apply(target: Value, motive: Value, base: Value, step: Value): Value {
     if (target instanceof Exps.NilValue) {
       return base
-    } else if (target instanceof Exps.LiValue) {
-      const { head, tail } = target
+    }
 
+    if (target instanceof Exps.LiValue) {
       return Exps.ApCore.apply(
-        Exps.ApCore.apply(Exps.ApCore.apply(step, head), tail),
-        Exps.ListIndCore.apply(tail, motive, base, step)
+        Exps.ApCore.apply(Exps.ApCore.apply(step, target.head), target.tail),
+        Exps.ListIndCore.apply(target.tail, motive, base, step)
       )
-    } else if (target instanceof Exps.NotYetValue) {
-      const { t, neutral } = target
+    }
 
-      if (t instanceof Exps.ListValue) {
-        const elem_t = t.elem_t
-        const motive_t = list_ind_motive_t(elem_t)
-        const base_t = Exps.ApCore.apply(motive, new Exps.NilValue())
-        const step_t = list_ind_step_t(motive, elem_t)
-        return new Exps.NotYetValue(
-          Exps.ApCore.apply(motive, target),
-          new Exps.ListIndNeutral(
-            neutral,
-            new Normal(motive_t, motive),
-            new Normal(base_t, base),
-            new Normal(step_t, step)
-          )
-        )
-      } else {
-        throw InternalError.wrong_target_t(target.t, {
-          expected: [Exps.ListValue],
-        })
-      }
-    } else {
+    if (!(target instanceof Exps.NotYetValue)) {
       throw InternalError.wrong_target(target, {
         expected: [Exps.NilValue, Exps.LiValue],
       })
     }
+
+    if (!(target.t instanceof Exps.ListValue)) {
+      throw InternalError.wrong_target_t(target.t, {
+        expected: [Exps.ListValue],
+      })
+    }
+
+    const elem_t = target.t.elem_t
+    const motive_t = list_ind_motive_t(elem_t)
+    const base_t = Exps.ApCore.apply(motive, new Exps.NilValue())
+    const step_t = list_ind_step_t(motive, elem_t)
+    return new Exps.NotYetValue(
+      Exps.ApCore.apply(motive, target),
+      new Exps.ListIndNeutral(
+        target.neutral,
+        new Normal(motive_t, motive),
+        new Normal(base_t, base),
+        new Normal(step_t, step)
+      )
+    )
   }
 }
