2467a8bdfcaa402e7a71e0fa6a7db76221c12897
Xie Yuheng <xyheme@gmail.com>
2022-04-14 08:06:44 +0800

    - `book.json` -- `authors` support `username`

--- src/types/book-config.ts
+++ src/types/book-config.ts
@@ -1,19 +1,29 @@
 import ty from "@xieyuheng/ty"
 
+type Author = string | { name: string; username: string }
+
 export interface BookConfig {
   title: string
   subtitle?: string
   version: string
   src?: string
-  authors?: Array<string>
+  authors?: Array<Author>
   date?: string
 }
+
+const AuthorSchema = ty.union(
+  ty.string(),
+  ty.object({
+    name: ty.string(),
+    username: ty.string(),
+  })
+)
 
 export const BookConfigSchema = ty.object<BookConfig>({
   title: ty.string(),
   subtitle: ty.optional(ty.string()),
   version: ty.semver(),
   src: ty.optional(ty.string()),
-  authors: ty.optional(ty.array(ty.string())),
+  authors: ty.optional(ty.array(AuthorSchema)),
   date: ty.optional(ty.string()),
 })
