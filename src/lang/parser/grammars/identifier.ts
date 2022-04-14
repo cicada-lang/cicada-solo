2979c22e60ba81da6e94fc867399bbbb6c608cb5
Xie Yuheng <xyheme@gmail.com>
2022-04-15 00:21:57 +0800

    - no need to preserve keywords

--- src/lang/parser/grammars/identifier.ts
+++ src/lang/parser/grammars/identifier.ts
@@ -1,17 +1,6 @@
 import pt from "@cicada-lang/partech"
 
 export const identifier = pt.grammars.pattern_unless_preserved("identifier", [
-  // keywords
-  "let",
-  "function",
-  "return",
-  "class",
-  "import",
-  "datatype",
-  "induction",
-  "recursion",
-  "check",
-  "same_as_chart",
   // built-in
   "car",
   "cdr",
